d7050c4465c854af9b4f1ef2604d4c5cc1dbc3df
James Hamilton <telcus@users.noreply.github.com>
2016-08-25 10:40:08 +1000

    Add a Title model
    Added a Title model to hold information about a run of issues. Although issues
    are contained within a title, they won't be held with the title model as this
    would mean all issues would have to be loaded into the model on load.
    
    Instead the Title has summary information about the titles (start and end year
    as well as issue count).

---
+++ src/classes/Model/Title.php
@@ -0,0 +1,142 @@
+<?php
+
+namespace ReadingLists\Model;
+
+/**
+ * A Title is a run of Issues that are referred to with a common name
+ *
+ * @package ReadingLists
+ * @subpackage Model
+ */
+class Title {
+    /** @var \ReadingLists\Type\Id The id of the title */
+    protected $title_id;
+    /** @var string The name of the title */
+    protected $name;
+    /** @var int|null The first year an issue was published for this title */
+    protected $start_year;
+    /** @var int|null The last year an issue was published for this title */
+    protected $end_year;
+    /** @var int The number of issues in the title */
+    protected $issue_count = 0;
+
+    /**
+     * Creates a new Title
+     *
+     * @param \ReadingLists\Type\Id $title_id
+     * @param string $name
+     * @param int|null $start_year
+     * @param int|null $end_year
+     * @param int $issue_count
+     *
+     * @return null
+     */
+    public function __construct(\ReadingLists\Type\Id $title_id, $name, $start_year = null, $end_year = null, $issue_count = 0) {
+        $this->setTitleId($title_id);
+        $this->setName($name);
+        $this->setStartYear($start_year);
+        $this->setEndYear($end_year);
+        $this->setIssueCount($issue_count);
+    }
+
+    /**
+     * The id of the title
+     * 
+     * @return \ReadingLists\Type\Id
+     */
+    public function getTitleId() { return $this->title_id; }
+
+    /**
+     * The name of the title
+     *
+     * @return string
+     */
+    public function getName() { return $this->name; }
+
+    /**
+     * The start year of the title
+     *
+     * @return int
+     */
+    public function getStartYear() { return $this->start_year; }
+
+    /**
+     * The end year of the title
+     *
+     * @return int
+     */
+    public function getEndYear() { return $this->end_year; }
+
+    /**
+     * The number of issues in the title
+     *
+     * @return int
+     */
+    public function getIssueCount() { return $this->issue_count; }
+
+    /**
+     * Sets the reference number
+     *
+     * @param \ReadingLists\Type\Id $title_id The new title reference
+     *
+     * @return null
+     */
+    protected function setTitleId(\ReadingLists\Type\Id $title_id) { $this->title_id = $title_id; }
+
+    /**
+     * Sets the name of the title
+     *
+     * @param string $name The new name
+     *
+     * @return null
+     */
+    public function setName($name) { $this->name = $name; }
+
+    /**
+     * Sets the first published year of the title
+     *
+     * @param int|null $start_year The new start year
+     *
+     * @return null
+     */
+    protected function setStartYear($start_year = null) { $this->start_year = $start_year; }
+
+    /**
+     * Sets the last published year of the title
+     *
+     * @param int|null $end_year The new end year
+     *
+     * @return null
+     */
+    protected function setEndYear($end_year = null) { $this->end_year = $end_year; }
+
+    /**
+     * Sets the number of issues in the title
+     *
+     * @param int $issue_count The new issue count
+     *
+     * @return null
+     */
+    protected function setIssueCount($issue_count) { $this->issue_count = $issue_count; }
+
+    /**
+     * Adds a new issue to the title run
+     *
+     * @param int $published_year The year the new issue was published
+     *
+     * @return null
+     */
+    public function addIssue($published_year) {
+        $this->setIssueCount($this->getIssueCount() + 1);
+
+        if ($this->getStartYear() == null || $published_year < $this->getStartYear()) {
+            $this->setStartYear($published_year);
+        }
+
+        if ($this->getEndYear() == null || $published_year > $this->getEndYear()) {
+            $this->setEndYear($published_year);
+        }
+    }
+
+}
+
